18316c1475e6eb31631cff1f325442f15f525e18
RealAmoe <116590501+RealAmoe@users.noreply.github.com>
2024-04-11 17:44:15 +0200

    Added task files

---
+++ Tutorial4/Controllers/AnimalsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tutorial4.Database;
+
+namespace Tutorial4.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AnimalsController : ControllerBase
+{
+    public AnimalsController()
+    {
+        
+    }
+    
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        // var animals = StaticData.animals;
+        
+        var animals = new MockDb().Animals;
+        return Ok(animals);
+    }
+}
---
+++ Tutorial4/Endpoints/AnimalEndpoints.cs
@@ -0,0 +1,32 @@
+﻿using Tutorial4.Models;
+
+namespace Tutorial4.Endpoints;
+
+public static class AnimalEndpoints
+{
+    public static void MapAnimalsEndpoints(this WebApplication app)
+    {
+        //GET
+        app.MapGet("/animals-minimalapi/{id}", (int id) =>
+        {
+            if (id != 1)
+            {
+                return Results.NotFound();
+            }
+            // process data
+            return Results.Ok();
+        });
+        // 200 - Ok
+        // 201 - Created
+        // 400 - Bad Request
+        // 404 - Not found
+
+        //POST    
+        app.MapPost("/animals-minimalapi", (Animal animal) =>
+        {
+    
+            // StaticData.animals.Add()
+            return Results.Created("", animal);
+        });
+    }
+}
